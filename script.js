57b1e728b6bdc195744d3f16d64d6c235510b6a6
leon gabriel <gabrielg.andrade77@gmail.com>
2026-03-19 17:09:32 -0300

    ver19/03

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
-        function comprarLista(event) {
-            event.preventDefault(); //impedir recarregar página NÃO ESQUEÇA
+function comprarLista(event) {
+    event.preventDefault(); //impedir recarregar página NÃO ESQUEÇA
 
-            const produtos = document.querySelector("#produtos").value;
+    const produtos = document.querySelector("#produtos").value;
 
-            const lista = document.querySelector("#lista");
+    const lista = document.querySelector("#lista");
 
-            const produtoLi = "<li>" + produtos + "</li>"; //elemento dinâmico.
-            lista.innerHTML = lista.innerHTML + produtoLi;
-            console.log(lista)
+    //create element
+    const produtoLi = document.createElement("li");
+    produtoLi.textContent = produtos;
+    lista.appendChild(produtoLi);
+
+    produtoLi.onclick = function () {
+        
+        const resposta = confirm("Você tem certeza que quer remover o item?")
+        console.log(resposta);
+        if (resposta == true){
+            produtoLi.remove();
         }
+
+    }
+    produtoLi.onmouseenter = function () {
+        produtoLi.textContent = "✏️ " + produtoLi.textContent;
+      }
+
+      produtoLi.onmouseleave = function () {
+        produtoLi.textContent = produtoLi.textContent.split(" ")[1];
+      }
+        
+
+    //InnetHTML.
+    //const produtoLi = "<li>" + produtos + "</li>"; 
+    //lista.innerHTML = lista.innerHTML + produtoLi;
+    //console.log(lista)
+}
