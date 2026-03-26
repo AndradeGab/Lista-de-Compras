3bc2bac497a793c9b657befccbf3dd02205c5811
leon gabriel <gabrielg.andrade77@gmail.com>
2026-03-26 14:13:18 -0300

    26/03

--- script.js
+++ script.js
@@ -1,35 +1,43 @@
 function comprarLista(event) {
-    event.preventDefault(); //impedir recarregar página NÃO ESQUEÇA
-
-    const produtos = document.querySelector("#produtos").value;
+    event.preventDefault();
 
     const lista = document.querySelector("#lista");
+    const input = document.querySelector("#produtos");
+    const produtoDigitado = input.value;
 
-    //create element
+    // cria o LI
     const produtoLi = document.createElement("li");
-    produtoLi.textContent = produtos;
+
+    // cria o texto
+    const spanProduto = document.createElement("span");
+    spanProduto.textContent = produtoDigitado;
+    produtoLi.appendChild(spanProduto);
+
+    // botão remover
+    const btnRemover = document.createElement("button");
+    btnRemover.textContent = "Remover";
+    btnRemover.onclick = function() {
+        produtoLi.remove();
+    };
+    produtoLi.appendChild(btnRemover);
+
+    // botão alterar
+    const btnAlterar = document.createElement("button");
+    btnAlterar.textContent = "Alterar";
+    btnAlterar.onclick = function() {
+        const novoProduto = prompt("Digite o novo nome");
+        spanProduto.textContent = novoProduto;
+    };
+    produtoLi.appendChild(btnAlterar);
+
+    // adiciona na lista
     lista.appendChild(produtoLi);
 
-    produtoLi.onclick = function () {
-        
-        const resposta = confirm("Você tem certeza que quer remover o item?")
-        console.log(resposta);
-        if (resposta == true){
-            produtoLi.remove();
-        }
-
-    }
-    produtoLi.onmouseenter = function () {
-        produtoLi.textContent = "✏️ " + produtoLi.textContent;
-      }
-
-      produtoLi.onmouseleave = function () {
-        produtoLi.textContent = produtoLi.textContent.split(" ")[1];
-      }
-        
+    // limpa input
+    input.value = "";
+}
 
     //InnetHTML.
     //const produtoLi = "<li>" + produtos + "</li>"; 
     //lista.innerHTML = lista.innerHTML + produtoLi;
     //console.log(lista)
-}
